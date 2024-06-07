045fef8dc3211512e8904e52c97f3c83067ad11b
djamet <kampretsiji1@gmail.com>
2024-06-07 10:05:22 +0700

    changes

--- dtokos/src/app/(user)/layout.tsx
+++ dtokos/src/app/(user)/layout.tsx
@@ -2,12 +2,16 @@ import LandingHeaderComponent from "@/components/landingHeader";
 import NavbarLanding from "@/components/navbarLanding";
 import React from "react";
 
-export default function LandingLayout({children}: {children: React.ReactNode}) {
-    return (
-        <div>
-            <LandingHeaderComponent />
-            {children}
-            <NavbarLanding />
-        </div>
-    )
+export default function LandingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className=" w-screen h-dvh">
+      <LandingHeaderComponent />
+      {children}
+      <NavbarLanding />
+    </div>
+  );
 }
